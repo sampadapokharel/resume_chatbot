d69faadf425cfe2fba29923df510b471c866f7cc
sampadapokharel <pokharelsampada@gmail.com>
2026-03-21 19:13:33 -0400

    add port information

--- chatbot.py
+++ chatbot.py
@@ -15,9 +15,11 @@ with open("system_prompt.txt", "r") as f:
 
 conn = psycopg2.connect(
     host=os.getenv("POSTGRES_HOST"),
+    port=os.getenv("POSTGRES_PORT", "5432"),
     database=os.getenv("POSTGRES_DB"),
     user=os.getenv("POSTGRES_USER"),
-    password=os.getenv("POSTGRES_PASSWORD")
+    password=os.getenv("POSTGRES_PASSWORD"),
+    sslmode="require"
 )
 
 register_vector(conn)
